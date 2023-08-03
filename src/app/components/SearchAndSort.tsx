908ec0cbfc6bd533b409390d226c1924a25a2f89
Rituja <rdixit@hagroup.com>
2023-08-03 17:04:11 +0530

    changing select box UI

--- src/app/components/SearchAndSort.tsx
+++ src/app/components/SearchAndSort.tsx
@@ -20,7 +20,12 @@ export default function SearchAndSort({ handleFiltersChange }: Props) {
 
             <div>
                 <label htmlFor="sort-by">Sort by:</label>
-                <select name="sort" id="sort-by" onChange={handleFiltersChange}>
+                <select
+                    name="sort"
+                    id="sort-by"
+                    onChange={handleFiltersChange}
+                    className="rounded-lg shadow-lg p-2 mx-4 bg-slate-50 border border-gray-500"
+                >
                     <option value="">--Please choose an option--</option>
                     <option value="low-to-high">Price - low to high</option>
                     <option value="high-to-low">Price - high to low</option>
